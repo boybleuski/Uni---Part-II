5710c699695b788a710823afbee0f333793bbef2
Sam Dolbel <samdolbel91@hotmail.com>
2022-03-11 23:54:58 +1100

    The rest of Games Production.

---
+++ Semester 2 - 2020/INFT3960 - Games Production/Assignments/Assignment3/TheGeomancer/Assets/Scripts/Fireball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public Transform fire_pos;
+    public GameObject firePrefab;
+    public OscarController player;
+
+    void Update()
+    {
+        // only fire if player has Fire power
+        if (Input.GetKeyDown(KeyCode.C) && player.hasFire)
+        {
+            ThrowFireball();
+            player.hasFire = false;
+        }
+
+        void ThrowFireball()
+        {
+            GameObject fb = Instantiate(firePrefab, fire_pos.position, fire_pos.rotation);
+            // Injects a function call into another script
+            fb.SendMessage("Prepare", player.facingRight);
+        }
+    }
+}
---
+++ Semester 2 - 2020/INFT3960 - Games Production/Assignments/Assignment3/TheGeomancer/Assets/Scripts/Lantern.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // if player, absorb fire power
+        if (coll.CompareTag("Player"))
+        {
+            OscarController player = coll.GetComponent<OscarController>();
+            player.ClaimFire();
+        }
+    }
+}
