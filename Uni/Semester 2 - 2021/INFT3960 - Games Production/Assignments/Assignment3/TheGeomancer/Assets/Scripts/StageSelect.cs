32dc626a157c9af0451968e2621fb76a81035371
Sam Dolbel <samdolbel91@hotmail.com>
2022-03-12 13:40:44 +1100

    Rearranged Games Production folder.

---
+++ Uni/Semester 2 - 2021/INFT3960 - Games Production/Assignments/Assignment3/TheGeomancer/Assets/Scripts/StageSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour
+{
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+}
